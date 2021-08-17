5aeaa9c7243a8af8f282341056ed571a819d6d70
Adam-smh <africa.adam1@gmail.com>
2021-08-17 21:38:48 +0200

    add products done

--- JS/add.js
+++ JS/add.js
@@ -1,3 +1,19 @@
+function File(preview, input) {
+  const file = input.files[0];
+  const reader = new FileReader();
+  reader.addEventListener(
+    "load",
+    function () {
+      preview.src = reader.result;
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
 function fetchProducts(url) {
   fetch(url, {
     method: "GET",
@@ -10,7 +26,7 @@ function fetchProducts(url) {
     .then((data) => {
       console.log(data);
       products = data.products;
-      let container = document.querySelector(".Container");
+      let container = document.querySelector(".productcontainer");
       container.innerHTML = "";
       products.forEach((product) => {
         container.innerHTML += `<div class="product" id="${product[0]}">
@@ -18,12 +34,14 @@ function fetchProducts(url) {
                                         <h6 class="productName">${product[2]}</h6>
                                         <p class="price">R${product[6]}</p>
                                         <div class="buttonContainer">
+                                        <button class="ugh" onclick="deleteP(this)">Delete</button>
                                         </div>
                                       </div>`;
       });
       let productElements = document.querySelectorAll(".product");
       productElements.forEach((product) => {
         product.addEventListener("click", (e) => {
+          toggleModal();
           fetch(
             `https://powerful-cove-00684.herokuapp.com/view-product/${e.currentTarget.id}`
           )
@@ -35,7 +53,7 @@ function fetchProducts(url) {
               document.querySelector(".modal").id = product[0];
               document.querySelector(".modal").innerHTML = `
                 <button class="closeM" onclick="toggleModal()">X</button>
-                <img class="mImg" src="${product[3]}" alt="Image preview...">
+                <img class="mImg" src="${product[3]}" alt="">
                 <div class="modalstuff">
                   <div class="info">
                     <h3>Name</h3>
@@ -49,8 +67,8 @@ function fetchProducts(url) {
                     <input class="mProductPrice" type="number" placeholder="Price" value="${product[6]}"/>
                     
                     <h3>Image</h3>
-                    <input class="mProductImage" type="file" onchange="previewFile(document.querySelector('.modalImg'))" />
-                    <button class="submitEdit" onclick="editProduct(document.querySelector('.modal'))">Save Changes</button>
+                    <input class="mProductImage" type="file" onchange="File(document.querySelector('.mImg'), document.querySelector(".mProductImage))" />
+                    <button class="submitEdit" onclick="edit(document.querySelector('.modal'))">Save Changes</button>
                   </div>
                 </div>
                 `;
@@ -65,10 +83,106 @@ fetchProducts(
 );
 
 function toggleModal() {
-  document.querySelector(".");
+  document.querySelector(".modalouter").classList.toggle("active");
 }
 
 function edit(e) {
   let product_id = parseInt(e.id);
-  let name = document.querySelector(".modalPro");
+  let name = document.querySelector(".mProductName").value;
+  let cat = document.querySelector(".mProductCategory").value;
+  let desc = document.querySelector(".mProductDescription").value;
+  let price = document.querySelector(".mProductPrice").value;
+  let img = document.querySelector(".mImg").src;
+
+  console.log(name, cat, desc, price, img, product_id);
+
+  fetch(
+    `https://powerful-cove-00684.herokuapp.com/edit-product/${product_id}/`,
+
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `jwt ${window.localStorage["jwt-token"]}`,
+      },
+      body: JSON.stringify({
+        product_name: name,
+        product_image: img,
+        product_category: cat,
+        product_description: desc,
+        product_price: price,
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      location.reload();
+    });
 }
+
+function deleteP(e) {
+  fetch(
+    `https://powerful-cove-00684.herokuapp.com/delete-product/${e.parentElement.parentElement.id}/`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `jwt ${window.localStorage["jwt-token"]}`,
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function add() {
+  let name = document.querySelector(".iName").value;
+  let img = document.querySelector(".dImg").src;
+  let cat = document.querySelector(".iCategory").value;
+  let desc = document.querySelector(".iDescription").value;
+  let price = document.querySelector(".iPrice").value;
+
+  console.log(name, cat, desc, price);
+
+  if (!name || !img || !cat || !desc || !price) {
+    alert("please enter all fields");
+    return;
+  }
+  fetch(
+    `https://powerful-cove-00684.herokuapp.com/add-product/${parseInt(
+      window.localStorage["user-id"]
+    )}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `jwt ${window.localStorage["jwt-token"]}`,
+      },
+      body: JSON.stringify({
+        product_name: name,
+        product_image: img,
+        product_category: cat,
+        product_description: desc,
+        product_price: price,
+      }),
+    }
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("please use different image");
+      }
+      res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // if (data.status_code != 200) {
+      //   alert(data.message);
+      // }
+    });
+}
+let form = document.querySelector(".idk");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  add();
+});
